a303ea6248accf42bf3c80e89226cf23eadad680
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:25:17 +0100

    Aggiunta 1.19.2-Fabric

---
+++ src/main/java/com/blocklegend001/onlyhammers/OnlyHammers.java
@@ -0,0 +1,16 @@
+package com.blocklegend001.onlyhammers;
+
+import com.blocklegend001.onlyhammers.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class OnlyHammers implements ModInitializer {
+    public static final String MOD_ID = "onlyhammers";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ModItems.registerModItems();
+    }
+}
