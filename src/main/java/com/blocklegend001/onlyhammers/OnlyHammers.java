5ecf8da8e2c090bcd660536175e9e93dedb0d5e2
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-24 15:22:12 +0200

    Added Highlights Outline to the Blocks

--- src/main/java/com/blocklegend001/onlyhammers/OnlyHammers.java
+++ src/main/java/com/blocklegend001/onlyhammers/OnlyHammers.java
@@ -3,7 +3,6 @@ package com.blocklegend001.onlyhammers;
 import com.blocklegend001.onlyhammers.config.ModConfigs;
 import com.blocklegend001.onlyhammers.item.ModItemGroup;
 import com.blocklegend001.onlyhammers.item.ModItems;
-import com.blocklegend001.onlyhammers.utils.HammerOverlayRenderer;
 import com.blocklegend001.onlyhammers.utils.HammerUsageEvent;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
@@ -19,7 +18,6 @@ public class OnlyHammers implements ModInitializer {
         ModConfigs.registerConfigs();
         ModItemGroup.registerItemGroups();
         ModItems.registerModItems();
-        HammerOverlayRenderer.init();
         PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
     }
 }
--- src/main/java/com/blocklegend001/onlyhammers/OnlyHammersClient.java
+++ src/main/java/com/blocklegend001/onlyhammers/OnlyHammersClient.java
@@ -1,10 +1,11 @@
 package com.blocklegend001.onlyhammers;
 
+import com.blocklegend001.onlyhammers.utils.HammerOverlayRenderer;
 import net.fabricmc.api.ClientModInitializer;
 
 public class OnlyHammersClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        HammerOverlayRenderer.init();
     }
 }
