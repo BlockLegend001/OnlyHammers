2db07ba85f941a7eedaaa72371007b9c45051088
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-24 19:55:49 +0200

    Added Highlights Outline to the Blocks and mores

--- src/main/java/com/blocklegend001/onlyhammers/item/custom/Hammer.java
+++ src/main/java/com/blocklegend001/onlyhammers/item/custom/Hammer.java
@@ -1,14 +1,12 @@
 package com.blocklegend001.onlyhammers.item.custom;
 
+import com.blocklegend001.onlyhammers.item.ModToolMaterials;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 
@@ -16,16 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hammer extends Item {
-    private static TagKey<Block> pickaxeMineable;
 
-    public Hammer(ToolMaterial material, float attackDamage, float attackSpeed, Item.Properties settings) {
-        super(computeSettings(material, BlockTags.MINEABLE_WITH_PICKAXE, settings, attackDamage, attackSpeed));
-    }
-
-    private static Item.Properties computeSettings(ToolMaterial material, TagKey<Block> pickaxeMineable, Item.Properties settings, float attackDamage, float attackSpeed) {
-        Hammer.pickaxeMineable = pickaxeMineable;
-        settings.pickaxe(wrapMaterial(material, material.durability()), attackDamage, attackSpeed);
-        return settings;
+    public Hammer(ModToolMaterials material, float attackDamage, float attackSpeed, Properties settings) {
+        super(material.applyPickaxeProperties(settings, BlockTags.MINEABLE_WITH_PICKAXE, attackDamage, attackSpeed));
     }
 
     public static List<BlockPos> getBlocksToBeDestroyed(int range, BlockPos initalBlockPos, ServerPlayer player) {
@@ -63,16 +54,5 @@ public class Hammer extends Item {
         }
 
         return positions;
-    }
-
-    private static ToolMaterial wrapMaterial(ToolMaterial toolMaterial, int durability) {
-        return new ToolMaterial(
-                toolMaterial.incorrectBlocksForDrops(),
-                durability,
-                toolMaterial.speed(),
-                toolMaterial.attackDamageBonus(),
-                toolMaterial.enchantmentValue(),
-                toolMaterial.repairItems()
-        );
     }
 }
