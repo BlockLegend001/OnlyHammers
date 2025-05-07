ad7f34c6c810c82b80064caeb312dbcdd7d910d8
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-07 19:12:03 +0200

    If the player is sneaking (shift held), the Excavator will only break the single targeted block.

--- src/main/java/com/blocklegend001/onlyhammers/mixin/LevelRendererMixin.java
+++ src/main/java/com/blocklegend001/onlyhammers/mixin/LevelRendererMixin.java
@@ -2,31 +2,30 @@ package com.blocklegend001.onlyhammers.mixin;
 
 import com.blocklegend001.onlyhammers.item.custom.Hammer;
 import com.blocklegend001.onlyhammers.utils.HammerOverlayRender;
+import com.mojang.blaze3d.resource.GraphicsResourceAllocator;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Camera;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.*;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraft.core.Direction;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import com.mojang.blaze3d.resource.GraphicsResourceAllocator;
-import net.minecraft.client.DeltaTracker;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
     @Inject(method = "renderLevel", at = @At("TAIL"))
-    private void renderLevelAfter(GraphicsResourceAllocator p_367325_, DeltaTracker p_342180_, boolean p_109603_, Camera p_109604_, GameRenderer p_109605_, Matrix4f p_254120_, Matrix4f p_330527_, CallbackInfo ci) {
+    private void renderLevelAfter(GraphicsResourceAllocator p_367325_, DeltaTracker p_342180_, boolean p_109603_, Camera p_109604_, GameRenderer p_109605_, LightTexture p_109606_, Matrix4f p_254120_, Matrix4f p_330527_, CallbackInfo ci) {
 
         if (Minecraft.getInstance().level == null || Minecraft.getInstance().player == null) {
             return;
@@ -43,7 +42,13 @@ public class LevelRendererMixin {
 
         BlockPos origin = blockHit.getBlockPos();
         Direction side = blockHit.getDirection();
-        int range = 1;
+        int range;
+        if (Minecraft.getInstance().player.isShiftKeyDown()) {
+            range = 0;
+        } else {
+            range = 1;
+        }
+
 
         if (!Minecraft.getInstance().level.getBlockState(origin).is(BlockTags.MINEABLE_WITH_PICKAXE)) {
             return;
