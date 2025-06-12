ebee155254bd0fabd559b933091d4c0dbdec4645
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-13 00:01:29 +0200

    Configurable hammer radius + tooltip display

--- src/main/java/com/blocklegend001/onlyhammers/ModConfigs.java
+++ src/main/java/com/blocklegend001/onlyhammers/ModConfigs.java
@@ -14,16 +14,27 @@ public class ModConfigs {
     private static final File CONFIG_FILE = new File("config/onlyhammers-common.toml");
 
     // Configurazioni per gli Hammers
-    public static int DurabilityWoodenHammer = 302;  // Durabilità dell'hammmer di legno
-    public static int DurabilityStoneHammer = 650;   // Durabilità dell'hammmer di pietra
-    public static int DurabilityIronHammer = 1300;    // Durabilità dell'hammmer di ferro
-    public static int DurabilityGoldHammer = 750;     // Durabilità dell'hammmer d'oro
-    public static int DurabilityLapisHammer = 1100;   // Durabilità dell'hammmer di lapislazzuli
-    public static int DurabilityRedstoneHammer = 1100; // Durabilità dell'hammmer di redstone
-    public static int DurabilityObsidianHammer = 11200; // Durabilità dell'hammmer di ossidiana
-    public static int DurabilityDiamondHammer = 8025;  // Durabilità dell'hammmer di diamante
-    public static int DurabilityEmeraldHammer = 9768;   // Durabilità dell'hammmer di smeraldo
-    public static int DurabilityNetheriteHammer = 13675; // Durabilità dell'hammmer di netherite
+    public static int DurabilityWoodenHammer = 302;
+    public static int DurabilityStoneHammer = 650;
+    public static int DurabilityIronHammer = 1300;
+    public static int DurabilityGoldHammer = 750;
+    public static int DurabilityLapisHammer = 1100;
+    public static int DurabilityRedstoneHammer = 1100;
+    public static int DurabilityObsidianHammer = 11200;
+    public static int DurabilityDiamondHammer = 8025;
+    public static int DurabilityEmeraldHammer = 9768;
+    public static int DurabilityNetheriteHammer = 13675;
+
+    public static int radiusWoodenHammer = 1;
+    public static int radiusStoneHammer = 1;
+    public static int radiusIronHammer = 1;
+    public static int radiusGoldHammer = 1;
+    public static int radiusLapisHammer = 1;
+    public static int radiusRedstoneHammer = 1;
+    public static int radiusObsidianHammer = 1;
+    public static int radiusDiamondHammer = 1;
+    public static int radiusEmeraldHammer = 1;
+    public static int radiusNetheriteHammer = 1;
 
     public static void loadConfig() {
         if (!CONFIG_FILE.exists()) {
@@ -42,6 +53,17 @@ public class ModConfigs {
                 DurabilityDiamondHammer = config.get("DurabilityDiamondHammer").getAsInt();
                 DurabilityEmeraldHammer = config.get("DurabilityEmeraldHammer").getAsInt();
                 DurabilityNetheriteHammer = config.get("DurabilityNetheriteHammer").getAsInt();
+
+                radiusWoodenHammer = config.get("radiusWoodenHammer").getAsInt();
+                radiusStoneHammer = config.get("radiusStoneHammer").getAsInt();
+                radiusIronHammer = config.get("radiusIronHammer").getAsInt();
+                radiusGoldHammer = config.get("radiusGoldHammer").getAsInt();
+                radiusLapisHammer = config.get("radiusLapisHammer").getAsInt();
+                radiusRedstoneHammer = config.get("radiusRedstoneHammer").getAsInt();
+                radiusObsidianHammer = config.get("radiusObsidianHammer").getAsInt();
+                radiusDiamondHammer = config.get("radiusDiamondHammer").getAsInt();
+                radiusEmeraldHammer = config.get("radiusEmeraldHammer").getAsInt();
+                radiusNetheriteHammer = config.get("radiusNetheriteHammer").getAsInt();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -61,6 +83,17 @@ public class ModConfigs {
         config.addProperty("DurabilityDiamondHammer", DurabilityDiamondHammer);
         config.addProperty("DurabilityEmeraldHammer", DurabilityEmeraldHammer);
         config.addProperty("DurabilityNetheriteHammer", DurabilityNetheriteHammer);
+
+        config.addProperty("radiusWoodenHammer", radiusWoodenHammer);
+        config.addProperty("radiusStoneHammer", radiusStoneHammer);
+        config.addProperty("radiusIronHammer", radiusIronHammer);
+        config.addProperty("radiusGoldHammer", radiusGoldHammer);
+        config.addProperty("radiusLapisHammer", radiusLapisHammer);
+        config.addProperty("radiusRedstoneHammer", radiusRedstoneHammer);
+        config.addProperty("radiusObsidianHammer", radiusObsidianHammer);
+        config.addProperty("radiusDiamondHammer", radiusDiamondHammer);
+        config.addProperty("radiusEmeraldHammer", radiusEmeraldHammer);
+        config.addProperty("radiusNetheriteHammer", radiusNetheriteHammer);
 
         try (FileWriter writer = new FileWriter(CONFIG_FILE)) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
