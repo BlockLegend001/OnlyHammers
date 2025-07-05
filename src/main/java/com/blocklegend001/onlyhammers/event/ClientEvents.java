34a8728abafa14498f3bb47034f95dbfeed07b46
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-05 10:58:26 +0200

    Added 1.21.7-Forge

---
+++ src/main/java/com/blocklegend001/onlyhammers/event/ClientEvents.java
@@ -0,0 +1,18 @@
+package com.blocklegend001.onlyhammers.event;
+
+import com.blocklegend001.onlyhammers.OnlyHammers;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = OnlyHammers.MOD_ID, value = Dist.CLIENT)
+public class ClientEvents {
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (Minecraft.getInstance().player != null) {
+            ModEvents.isSneaking = Minecraft.getInstance().player.isShiftKeyDown();
+        }
+    }
+}
