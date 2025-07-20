3440da4550b5ef174bbaf54549d42d9d9a769656
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-20 17:17:21 +0200

    Fixed a bug that not let the player break one block when shifting in multiplayer

--- src/main/java/com/blocklegend001/onlyhammers/event/ModEvents.java
+++ src/main/java/com/blocklegend001/onlyhammers/event/ModEvents.java
@@ -17,7 +17,6 @@ import java.util.Set;
 @EventBusSubscriber(modid = OnlyHammers.MOD_ID)
 public class ModEvents {
     private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
-    public static boolean isSneaking = false;
 
     @SubscribeEvent
     public static boolean onExcavatorUsage(BlockEvent.BreakEvent event) {
@@ -25,18 +24,18 @@ public class ModEvents {
         ItemStack mainHandItem = player.getMainHandItem();
 
         if (!(player instanceof ServerPlayer serverPlayer)) return true;
-
         if (!(mainHandItem.getItem() instanceof Hammer excavator)) return true;
-
         if (HARVESTED_BLOCKS.contains(event.getPos())) return true;
+
+        boolean isSneaking = player.isCrouching() || player.isShiftKeyDown();
 
         HARVESTED_BLOCKS.add(event.getPos());
 
         try {
             int radius = isSneaking ? 0 : RadiusMap.getHammerRadius().get(mainHandItem.getItem());
+
             for (BlockPos targetPos : Hammer.getBlocksToBeDestroyed(radius, event.getPos(), serverPlayer)) {
                 if (targetPos.equals(event.getPos())) continue;
-
                 if (HARVESTED_BLOCKS.contains(targetPos)) continue;
                 if (!excavator.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(targetPos))) continue;
 
@@ -47,7 +46,6 @@ public class ModEvents {
         } finally {
             HARVESTED_BLOCKS.remove(event.getPos());
         }
-
         return true;
     }
 }
