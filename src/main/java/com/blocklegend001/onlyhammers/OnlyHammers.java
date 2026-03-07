083abd6d2fff8da8fbab196c690f863ed9593fc9
BlockLegend001 <rosarioarcamone@gmail.com>
2026-03-07 20:01:07 +0100

    Added KeyBind to Enable/Disable the Outline

--- src/main/java/com/blocklegend001/onlyhammers/OnlyHammers.java
+++ src/main/java/com/blocklegend001/onlyhammers/OnlyHammers.java
@@ -2,7 +2,11 @@ package com.blocklegend001.onlyhammers;
 
 import com.blocklegend001.onlyhammers.item.ModCreativeModeTabs;
 import com.blocklegend001.onlyhammers.item.ModItems;
+import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -10,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 
 @Mod(OnlyHammers.MOD_ID)
@@ -17,6 +22,20 @@ public class OnlyHammers {
 
     public static final String MOD_ID = "onlyhammers";
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static boolean SHOW_OUTLINE_ENABLED = true;
+    public static final KeyMapping.Category ONLYHAMMERS_CATEGORY =
+            KeyMapping.Category.register(
+                    ResourceLocation.fromNamespaceAndPath(MOD_ID, "showoutline")
+            );
+
+    public static final Lazy<KeyMapping> SHOW_OUTLINE_KEY = Lazy.of(() ->
+            new KeyMapping(
+                    "key.onlyhammers.showoutline",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_O,
+                    ONLYHAMMERS_CATEGORY
+            )
+    );
 
     public OnlyHammers(FMLJavaModLoadingContext context) {
         var modBusGroup = context.getModBusGroup();
