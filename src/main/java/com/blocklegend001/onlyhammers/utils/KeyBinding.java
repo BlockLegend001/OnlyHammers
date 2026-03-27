c36f883162bf6a5557626722cf1bd7d0042cf0fa
BlockLegend001 <rosarioarcamone@gmail.com>
2026-03-27 10:36:06 +0100

    Fixed a bug that let the server crash with the latest update

--- src/main/java/com/blocklegend001/onlyhammers/utils/KeyBinding.java
+++ src/main/java/com/blocklegend001/onlyhammers/utils/KeyBinding.java
@@ -1,16 +1,27 @@
 package com.blocklegend001.onlyhammers.utils;
 
 import com.blocklegend001.onlyhammers.OnlyHammers;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = OnlyHammers.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 
 public class KeyBinding {
+
+    public static final KeyMapping SHOW_OUTLINE_KEY = new KeyMapping(
+            "key.onlyhammers.showoutline",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_O,
+            "key.category.onlyhammers.showoutline"
+    );
+
     @SubscribeEvent
     public static void registerKeys(RegisterKeyMappingsEvent event) {
-        event.register(OnlyHammers.SHOW_OUTLINE_KEY.get());
+        event.register(SHOW_OUTLINE_KEY);
     }
 }
