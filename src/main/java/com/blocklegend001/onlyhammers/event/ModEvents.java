e7b51050334a469c1227e102dc0812287d866520
BlockLegend001 <rosarioarcamone@gmail.com>
2026-09-05 17:03:03 +0200

    Small bug fix

--- src/main/java/com/blocklegend001/onlyhammers/event/ModEvents.java
+++ src/main/java/com/blocklegend001/onlyhammers/event/ModEvents.java
@@ -8,7 +8,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.HashSet;
